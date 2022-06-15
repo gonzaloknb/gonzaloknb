20819eb23746a86466d20b618b5e41d265e6638e
Diego Unanue <diego.unanue@commonbond.co>
2022-06-15 19:05:59 -0300

    Agregando arrays y  objetos

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ let pulseraSimilCobre = 750;
 
 let cantidadTotal = 0;
 
+let producto1 = {
+    id: 1,
+    nombre: "musculosa estampada",
+    precio: 1550
+};
+
+let productos = [
+    {   
+        id: 2,
+        nombre: "remera volados",
+        precio: 1700,
+    },
+    {
+        id: 3,
+        nombre: "pulsera simil cobre",
+        precio: 750,
+    },
+];
+
+productos.push(producto1);
+
 function calcularSubTotal(precioProducto, nombreProducto) {
     cantidad = +prompt("Ingrese cantidad de" + nombreProducto);
     cantidadTotal = cantidadTotal + cantidad;
@@ -14,20 +35,35 @@ function calcularSubTotal(precioProducto, nombreProducto) {
 }
 
 function calcularTotal() {
-    let subTotalMusculosaEstampada = calcularSubTotal(precioMuscuosaEstampada, "musculosa estampada");
-    let subTotalRemeraVolados = calcularSubTotal(precioRemeraVolados, "remera volados");
-    let subTotalPulseraSimilCobre = calcularSubTotal(pulseraSimilCobre, "pulsera simil cobre");
-    console.log('cantidad :>> ', cantidad);
+//    let subTotalMusculosaEstampada = calcularSubTotal(precioMuscuosaEstampada, "musculosa estampada");
+//   let subTotalRemeraVolados = calcularSubTotal(precioRemeraVolados, "remera volados");
+//   let subTotalPulseraSimilCobre = calcularSubTotal(pulseraSimilCobre, "pulsera simil cobre");
+    let total = 0;
+
+
+    for(const item of productos){
+        let subtotal = calcularSubTotal(item.precio, item.nombre);
+        total = total + subtotal;
+        console.log("subtotal de $" + subtotal);
+        let mostrarSubTotal = "SUBTOTAL DE " + item.nombre + ": $" + subtotal;
+        alert(mostrarSubTotal);
+    }
+
     if(cantidadTotal > 3){
         let descuento = 15;
-        let descuentoTotal = (subTotalMusculosaEstampada + subTotalRemeraVolados + subTotalPulseraSimilCobre) * descuento / 100;
-        console.log('descuentoTotal :>> ', descuentoTotal);
-        return subTotalMusculosaEstampada + subTotalRemeraVolados + subTotalPulseraSimilCobre - descuentoTotal;
+        let descuentoTotal = total * descuento / 100;
+        let mostrarTotal = "El precio total con descuento de $" + descuentoTotal + " es de $" + (total - descuentoTotal);
+        alert(mostrarTotal);
+        return total - descuentoTotal;
     }
-    return subTotalMusculosaEstampada + subTotalRemeraVolados + subTotalPulseraSimilCobre;
-}
 
-console.log("El precio total es $", calcularTotal());
+    alert("El precio total es $", total );
+    return total;
+}
+calcularTotal();
+
+
+
 
 
 
